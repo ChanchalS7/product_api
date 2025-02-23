597549eba713fb3316683c7749c0d1a984aa5d6f
ChanchalS7 <chsverma7@gmail.com>
2025-02-23 17:27:16 +0530

    initialized database

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/ChanchalS7/product_api/config"
+	"github.com/ChanchalS7/product_api/middleware"
+	"github.com/ChanchalS7/product_api/routes"
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	config.LoadEnv()
+	router := mux.NewRouter()
+	//middleware
+	router.Use(middleware.Logging)
+
+	//Routes
+	routes.RegisterAuthRoutes(router)
+	routes.RegisterProductRoutes(router)
+
+	port := os.Getenv("PORT")
+	logrus.Info("Server starting on port:", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+
+}
